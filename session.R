27a073c6843a482ea3a28d8e0f2db8410b126cbc
Muness Castle <muness@gmail.com>
2014-09-20 21:44:06 -0400

    Q3

--- session.R
+++ session.R
@@ -1,15 +1,12 @@
-## This first line will likely take a few seconds. Be patient!
 NEI <- readRDS("summarySCC_PM25.rds")
 SCC <- readRDS("Source_Classification_Code.rds")
 
-head(NEI)
-
-class(NEI)
-
 library(reshape2)
-NEI_melt_by_year <- melt(NEI, id.vars="year", measure.vars="Emissions") 
 
-head(NEI_melt_by_year)
+# 1.Have total emissions from PM2.5 decreased in the United States from 1999 to 2008? Using the base plotting system, make a plot showing the total PM2.5 emission from all sources for each of the years 1999, 2002, 2005, and 2008.
+
+
+NEI_melt_by_year <- melt(NEI, id.vars="year", measure.vars="Emissions") 
 NEI_pivot_by_year <- dcast(NEI_melt_by_year, year ~ variable, sum) 
 
 png("plot1.png")
@@ -17,5 +14,27 @@ plot(NEI_pivot_by_year, type="p", main="Emission by year")
 # this shows that there has been a drop in emmissions from 1999 to 2008
 dev.off()
 
-NEI_pivot_balt_by_year <- melt(subset(NEI, fips=="", id.vars=c("fips", "year"), measure.vars= "Emissions")
-head(NEI_pivot_balt_by_year)
+# 2.Have total emissions from PM2.5 decreased in the Baltimore City, Maryland (fips == "24510") from 1999 to 2008? Use the base plotting system to make a plot answering this question.
+
+
+NEI_pivot_balt_by_year <- recast(subset(NEI, fips=="24510"), year ~ variable, sum, id.var="year", measure.var= "Emissions")
+
+png("plot2.png")
+plot(NEI_pivot_balt_by_year, type="p", main="Emission by year in baltimore")
+# this shows that there has been a drop in emmissions from 1999 to 2008 in Baltimore (despite a temp increase from 2002 to 2005)
+dev.off()
+
+# 3.Of the four types of sources indicated by the type (point, nonpoint, onroad, nonroad) variable, which of these four sources have seen decreases in emissions from 1999-2008 for Baltimore City? Which have seen increases in emissions from 1999-2008? Use the ggplot2 plotting system to make a plot answer this question.
+
+NEI_balt_melt_by_year_and_type <- melt(subset(NEI, fips=="24510"), id.vars=c("year","type"), measure.vars="Emissions")
+NEI_balt_cast_year_and_type <- dcast(NEI_balt_melt_by_year_and_type, year + type ~ variable, sum)
+
+library(ggplot2)
+png("plot3.png")
+p<-ggplot(NEI_balt_cast_year_and_type, aes(x=year, y=Emissions)) + geom_line()
+p + facet_wrap(~ type, scales="free_y")
+# Between 1999 and 2008, non-road, on-road, nonpoint have seen decreases in emissions. Point has seen an increase.
+dev.off()
+
+
+
