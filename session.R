9f0b50bfd71cf02288d72598e02edc99bb8ecad1
Muness Castle <muness@gmail.com>
2014-09-21 01:23:29 -0400

    cleanup

--- session.R
+++ session.R
@@ -2,8 +2,6 @@ NEI <- readRDS("summarySCC_PM25.rds")
 SCC <- readRDS("Source_Classification_Code.rds")
 
 library(reshape2)
-
-
 # 1.Have total emissions from PM2.5 decreased in the United States from 1999 to 2008? Using the base plotting system, make a plot showing the total PM2.5 emission from all sources for each of the years 1999, 2002, 2005, and 2008.
 NEI_melt_by_year <- melt(NEI, id.vars="year", measure.vars="Emissions") 
 NEI_pivot_by_year <- dcast(NEI_melt_by_year, year ~ variable, sum) 
@@ -48,7 +46,7 @@ SCC_vehicles<-subset(SCC, grepl("Vehicle", EI.Sector))
 NEI_vehicles_balt <- recast(subset(NEI, fips=="24510" & SCC %in% SCC_vehicles$SCC), year ~ variable, sum, id.var="year", measure.var= "Emissions")
 
 png("plot5.png")
-plot(NEI_vehicles_balt, type="p", main="Vehicle emissions ")
+plot(NEI_vehicles_balt, type="p", main="Vehicle emissions in Baltimore")
 # this shows that there has been a drop in vehicle emissions in Baltimore from 1999 to 2008
 dev.off()
 
