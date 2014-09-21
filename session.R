953a5d83e12145fbbcfe5f51f4399be3b8042221
Muness Castle <muness@gmail.com>
2014-09-20 20:54:16 -0400

    Question 1

---
+++ session.R
@@ -0,0 +1,15 @@
+## This first line will likely take a few seconds. Be patient!
+NEI <- readRDS("summarySCC_PM25.rds")
+SCC <- readRDS("Source_Classification_Code.rds")
+
+head(NEI)
+
+class(NEI)
+
+library(reshape2)
+NEI_melt_by_year <- melt(NEI, id.vars = "year", measure.vars = "Emissions") 
+
+head(NEI_melt_by_year)
+NEI_pivot_by_year <- dcast(NEI_melt_by_year, year ~ variable, sum) 
+
+plot(NEI_pivot_by_year, type="p", main="Emission by year")
