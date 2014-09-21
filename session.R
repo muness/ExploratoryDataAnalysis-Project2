ece9fa34e08b21840706d160947d3c0ac5a4911c
Muness Castle <muness@gmail.com>
2014-09-20 22:44:07 -0400

    Q6

--- session.R
+++ session.R
@@ -10,7 +10,7 @@ NEI_pivot_by_year <- dcast(NEI_melt_by_year, year ~ variable, sum)
 
 png("plot1.png")
 plot(NEI_pivot_by_year, type="p", main="Emission by year")
-# this shows that there has been a drop in emmissions from 1999 to 2008
+# this shows that there has been a drop in emissions from 1999 to 2008
 dev.off()
 
 
@@ -19,7 +19,7 @@ NEI_pivot_balt_by_year <- recast(subset(NEI, fips=="24510"), year ~ variable, su
 
 png("plot2.png")
 plot(NEI_pivot_balt_by_year, type="p", main="Emission by year in baltimore")
-# this shows that there has been a drop in emmissions from 1999 to 2008 in Baltimore (despite a temp increase from 2002 to 2005)
+# this shows that there has been a drop in emissions from 1999 to 2008 in Baltimore (despite a temp increase from 2002 to 2005)
 dev.off()
 
 
@@ -39,8 +39,38 @@ SCC_coal<-subset(SCC, grepl("Coal", EI.Sector))
 NEI_coal_by_year <- recast(subset(NEI, SCC %in% SCC_coal$SCC), year ~ variable, sum, id.var="year", measure.var= "Emissions")
 png("plot4.png")
 plot(NEI_coal_by_year, type="p", main="Coal emissions by year")
-# this shows that there has been a drop in Coal emmissions from 1999 to 2008
+# this shows that there has been a drop in Coal emissions from 1999 to 2008
 dev.off()
 
 # 5.How have emissions from motor vehicle sources changed from 1999-2008 in Baltimore City? 
 
+SCC_vehicles<-subset(SCC, grepl("Vehicle", EI.Sector))
+NEI_vehicles_balt <- recast(subset(NEI, fips=="24510" & SCC %in% SCC_vehicles$SCC), year ~ variable, sum, id.var="year", measure.var= "Emissions")
+
+png("plot5.png")
+plot(NEI_vehicles_balt, type="p", main="Vehicle emissions ")
+# this shows that there has been a drop in vehicle emissions in Baltimore from 1999 to 2008
+dev.off()
+
+
+# 6.Compare emissions from motor vehicle sources in Baltimore City with emissions from motor vehicle sources in Los Angeles County, California (fips == "06037"). Which city has seen greater changes over time in motor vehicle emissions?
+NEI_melt_vehicles_balt_and_la <- melt(
+  subset(NEI, (fips=="24510" | fips =="06037") & SCC %in% SCC_vehicles$SCC), 
+  id.vars=c("fips", "year"), measure.var= "Emissions")
+
+NEI_cast_vehicle_balt_and_la_by_fips_and_year <- dcast(NEI_melt_vehicles_balt_and_la, fips + year ~ variable, sum)
+
+balt_and_la_labeller <- function(var, value) {
+  value<-as.character(value)
+  if (var=="fips") {
+    value[value=="06037"]<-"Los Angeles"
+    value[value=="24510"]<-"Baltimore"
+  }
+  return(value)
+}
+
+png("plot6.png")
+p<-ggplot(NEI_cast_vehicle_balt_and_la_by_fips_and_year, aes(x=year, y=Emissions)) + geom_line()
+p + facet_grid(~ fips, labeller=balt_and_la_labeller)
+# this shows that emissions have risen in LA (though they are dropping from their height in 2005) while dropping in Baltimore. LA emissions are much higher
+dev.off()
